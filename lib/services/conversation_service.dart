3f860695aeaa8438e980f2420579bd137271bf93
Andrej Sakal <andrej.sakal@motoroeldirekt.at>
2021-09-03 13:15:23 +0200

    camera functionality and view on chatscreen

--- lib/services/conversation_service.dart
+++ lib/services/conversation_service.dart
@@ -4,25 +4,26 @@ import 'package:chat_app/models/message_model.dart';
 import 'package:chat_app/services/base_api.dart';
 import 'package:http/http.dart' as http;
 
-class ConversationService  extends BaseApi{
+class ConversationService extends BaseApi {
   Future<http.Response> getConversations() async {
     return await api.httpGet('conversations');
   }
+
   Future<http.Response> getLastConversation() async {
     return await api.httpGet('lastconversation');
   }
-  Future<http.Response> newConversation(String userId, String message, String carId) async {
-    return await api.httpPost('conversations', {
-      'user_id':userId,
-      'message':message,
-      'car_id':carId
-    });
+
+  Future<http.Response> newConversation(
+      String userId, String message, String carId) async {
+    return await api.httpPost('conversations',
+        {'user_id': userId, 'message': message, 'car_id': carId});
   }
+
   Future<http.Response> storeMessage(MessageModel message) async {
     return await api.httpPost('messages', {
-      'body':message.body,
+      'body': message.body,
+      'image': message.image,
       'conversation_id': message.conversationId.toString()
-      }
-    );
+    });
   }
 }
